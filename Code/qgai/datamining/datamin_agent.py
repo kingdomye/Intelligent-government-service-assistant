b7006b08e21a55c45d996790d421ab0209426915
LZX <3267343261@qq.com>
2025-07-22 21:54:12 +0800

    0722_nigth

--- Code/qgai/datamining/datamin_agent.py
+++ Code/qgai/datamining/datamin_agent.py
@@ -1,11 +1,23 @@
 import json
 
+table_mark={
+    "<IMG>":0,
+    "<SIG>":1,
+    "<SEL>":2,
+    "<PAD>":3,
+    "<DAT>":4,
+}
+
 class DataMiningAgent:
-    def __init__(self, tables_path='tables.json', idx_path='name_to_idx.json'):
+    def __init__(self, tables_path='tables.json',
+                 idx_path='name_to_idx.json',
+                 flow_path='flows.json', ):
         with open(idx_path, 'r', encoding='utf-8') as f:
             self.name_to_idx = json.load(f)
         with open(tables_path, 'r', encoding='utf-8') as f:
             self.tables = json.load(f)
+        with open(flow_path, 'r', encoding='utf-8') as f:
+            self.flows = json.load(f)
 
     def label_to_idx(self, label: str)->int:
         """
@@ -34,8 +46,12 @@ class DataMiningAgent:
         return list(self.tables[idx].values())
 
     def get_flow(self, idx)->str:
-        return '114514'
+        idx = str(idx)
+        return self.flows[idx]
 
     def __getitem__(self, idx: int)->(str,list):
         return self.get_flow(idx), self.get_tables(idx)
 
+# 调试
+# data = DataMiningAgent()
+# print(data[0])
