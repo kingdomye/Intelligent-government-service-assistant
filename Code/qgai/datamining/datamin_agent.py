a408350cf76823cf79106c11120c66a01c8fefdd
LZX <3267343261@qq.com>
2025-07-23 22:23:38 +0800

    0723

--- Code/qgai/datamining/datamin_agent.py
+++ Code/qgai/datamining/datamin_agent.py
@@ -1,41 +1,84 @@
 import json
+import os
+
+table_mark={
+    "<IMG>":0,
+    "<SIG>":1,
+    "<SEL>":2,
+    "<PAD>":3,
+    "<DAT>":4,
+}
 
 class DataMiningAgent:
-    def __init__(self, tables_path='tables.json', idx_path='name_to_idx.json'):
+    def __init__(self, tables_path='tables.json',
+                 idx_path='name_to_idx.json',
+                 flow_path='flows.json', ):
+        assert os.path.exists(tables_path), f"{tables_path} does not exist"
+        assert os.path.exists(idx_path), f"{idx_path} does not exist"
+        assert os.path.exists(flow_path), f"{flow_path} does not exist"
+
         with open(idx_path, 'r', encoding='utf-8') as f:
             self.name_to_idx = json.load(f)
         with open(tables_path, 'r', encoding='utf-8') as f:
             self.tables = json.load(f)
+        with open(flow_path, 'r', encoding='utf-8') as f:
+            self.flows = json.load(f)
 
     def label_to_idx(self, label: str)->int:
         """
         Transform label to index
         :param label: key word of business like '身份证'
-        :return: index
+        :return: index / error:-1
         """
-        return self.name_to_idx[label]
+        try :
+            return self.name_to_idx[label]
+        except KeyError:
+            return -1
 
     def idx_to_label(self, idx: int)->str:
         """
         Transform idx to label
         :param idx: index of business
-        :return: label
+        :return: label / error:-1
         """
         reverse_dict = {v: k for k, v in self.name_to_idx.items()}
-        return reverse_dict[idx]
+        try:
+            return reverse_dict[idx]
+        except KeyError:
+            return '-1'
 
     def get_tables(self, idx: int)->list:
         """
         Get table by index
         :param idx: table index(view in 'name_to_idx.json')
-        :return: tables list (list of dicts from 'tables.json')
+        :return: tables list (list of dicts from 'tables.json') / error: ['-1']
         """
-        idx = str(idx)
-        return list(self.tables[idx].values())
+        try:
+            idx = str(idx)
+            return list(self.tables[idx].values())
+        except KeyError:
+            return ['-1']
 
     def get_flow(self, idx)->str:
-        return '114514'
+        """Get flow by index
+        :param idx: flow index(view in 'name_to_idx.json')
+        :return: flow / error:-1
+        """
+        try:
+            idx = str(idx)
+            return self.flows[idx]
+        except KeyError:
+            return '-1'
 
-    def __getitem__(self, idx: int)->(str,list):
+    def __getitem__(self, idx: int)->(str, list):
+        """
+        Get basic data by idx
+        :param idx: index of business
+        :return: flow: str, tables: list of dicts
+        """
         return self.get_flow(idx), self.get_tables(idx)
 
+
+# ## 调试
+# data = DataMiningAgent()
+# print(data[666])
